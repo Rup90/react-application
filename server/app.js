e63dbb89ccc4c9468fb739749eea3fcbf9af062b
Amrita Gupta <gupta1.amrita@gmail.com>
2020-07-11 21:32:58 +0530

    fixed API load issue

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
 var cors = require('cors');
 var indexRouter = require('./routes/index');
+const bodyParser = require("body-parser");
 // var usersRouter = require('./routes/users');
 
 var app = express();
@@ -12,24 +13,27 @@ var app = express();
 app.use(express.static(__dirname + '/views/react-frontend'));
 
 /*** CORS Configuration ***/
-var whitelist = ['http://localhost:3000'];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+// var whitelist = ['http://localhost:3000'];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions));
 /*** CORS Configuration ***/
+
+app.use(bodyParser.json({limit: '5000mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '5000mb', extended: true}));
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-// app.use(logger('dev'));
+// app.use(logger('dev'));ß
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -42,24 +46,24 @@ app.use(cookieParser());
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 app.use('/api', indexRouter);
-app.get('/*', (req, res) => {
-  const indexFile = path.resolve(__dirname + "/views/react-frontend/index.html");
-  res.sendfile(indexFile);
-})
+// app.get('/*', (req, res) => {
+//   const indexFile = path.resolve(__dirname + "/views/react-frontend/index.html");
+//   res.sendfile(indexFile);
+// })
 
 module.exports = app;
